f8ffb0c348b957044e29f9dc832f5f1c2cfc56d8
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2023-08-24 16:03:18 +0300

    updates

--- main.py
+++ main.py
@@ -6,63 +6,101 @@ from tqdm.auto import tqdm
 from timeit import default_timer as timer
 from torchinfo import summary
 
+from torch.autograd import Variable
+
 
 data_handler = DataHandler(data_path='data/causal_dataset.csv')
 
-data_handler.get_data_info()
-print(data_handler.get_data_shape())
-X_train, X_test, y_train, y_test = data_handler.get_data_tensors()
-print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+train_loader, test_loader = data_handler.get_dataloaders()
 
-model = ClassificationModel(input_size=X_train.shape[1], 
-                            hidden_units=20, 
+# data_handler.get_data_info()
+# print(data_handler.get_data_shape())
+# X_train, X_test, y_train, y_test = data_handler.get_data_tensors()
+# print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+model = ClassificationModel(input_size=train_loader.dataset.tensors[0].shape[1], 
+                            hidden_units=10, 
                             output_shape=1, 
                             dropout=True)
 
-model_summary = summary(model, input_size=(X_train.shape[1], ))
+model_summary = summary(model, input_size=(train_loader.dataset.tensors[0].shape[1], ))
 print(model_summary)
 
-EPOCHS = 100 
+EPOCHS = 300
 LEARNING_RATE = 0.001
 
-# define loss function and optimizer
-criterion = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+# # define loss function and optimizer
+criterion = nn.MSELoss(size_average=False)
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
+history = { "loss": [], "accuracy": [], "loss_val": [], "accuracy_val": [] }
 
-st = timer()
 # train model
 for epoch in range(EPOCHS):
-    model.train()
+    loss = 0
+    for idx, (minibatch, target) in enumerate(train_loader):
+        y_pred = model(Variable(minibatch))
 
-    # forward pass
-    y_pred = model(X_train)
+        loss = criterion(y_pred, Variable(target.float()))
+
+        # compute loss
+        loss += criterion(y_pred, Variable(target.unsqueeze(dim=1)))
+        prediction = [1 if x > 0.5 else 0 for x in y_pred.data.numpy()]
+        correct = (prediction == target.numpy()).sum()
+
+        # compute accuracy
+        acc = (y_pred == target.unsqueeze(dim=1)).sum() / target.shape[0]
+
+        # zero out gradients
+        optimizer.zero_grad()
+
+        # backward pass
+        loss.backward()
+
+        # update weights
+        optimizer.step()
+
+    history["loss"].append(loss.data[0])
+    history["accuracy"].append(100 * correct / len(prediction))
     
-    # compute loss
-    loss = criterion(y_pred, y_train.unsqueeze(dim=1))
-
-    # backward pass
-    loss.backward()
-
-    # zero out gradients
-    optimizer.zero_grad()
-
-    # update weights
-    optimizer.step()
-
     # print loss
     if epoch % 10 == 0:
-        print(f'Epoch: {epoch+1}, Loss: {loss.item():.3f}')
+        print(f'Epoch: {epoch+1}, Loss: {history["loss"][-1]:.3f}, Accuracy: {history["accuracy"][-1]:.3f}')
 
-et = timer()
 
-print(f'Time elapsed: {et-st:.2f} seconds')
+# st = timer()
+# # train model
+# for epoch in range(EPOCHS):
+#     model.train()
 
-# evaluate model
-model.eval()
-with torch.inference_mode():
-    y_pred = model(X_test)
-    y_pred = torch.sigmoid(y_pred)
-    y_pred = torch.round(y_pred)
-    acc = (y_pred == y_test.unsqueeze(dim=1)).sum() / y_test.shape[0]
-    print(f'Accuracy: {acc.item():.3f}')
+#     # forward pass
+#     y_pred = model(X_train)
+    
+#     # compute loss
+#     loss = criterion(y_pred, y_train.unsqueeze(dim=1))
+
+#     # backward pass
+#     loss.backward()
+
+#     # zero out gradients
+#     optimizer.zero_grad()
+
+#     # update weights
+#     optimizer.step()
+
+#     # print loss
+#     if epoch % 10 == 0:
+#         print(f'Epoch: {epoch+1}, Loss: {loss.item():.3f}')
+
+# et = timer()
+
+# print(f'Time elapsed: {et-st:.2f} seconds')
+
+# # evaluate model
+# model.eval()
+# with torch.inference_mode():
+#     y_pred = model(X_test)
+#     y_pred = torch.sigmoid(y_pred)
+#     y_pred = torch.round(y_pred)
+#     acc = (y_pred == y_test.unsqueeze(dim=1)).sum() / y_test.shape[0]
+#     print(f'Accuracy: {acc.item():.3f}')
--- src/data_handler.py
+++ src/data_handler.py
@@ -2,53 +2,91 @@ import pandas as pd
 import numpy as np
 from typing import Tuple
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import torch 
 from torch.utils.data import Dataset, DataLoader
 
 class DataHandler():
-    def __init__(self, data_path: str) -> None:
+    def __init__(self, 
+                 data_path: str, 
+                 BATCH_SIZE : int = 32) -> None:
         self.data_path = data_path
         self.data = self._load_data()
+        self.x_train = None
+        self.x_test = None
+        self.y_train = None
+        self.y_test = None
+        self.BATCH_SIZE = BATCH_SIZE
 
     def _load_data(self) -> pd.DataFrame:
         return pd.read_csv(self.data_path)
-
-    def _preprocess_data(self) -> pd.DataFrame:
-        self.data = self.data.dropna()
-        self.data = self.data.drop_duplicates()
-        self.data = self.data.reset_index(drop=True)
-        return self.data
-
-    def get_data_shape(self) -> Tuple[int, int]:
-        return self.data.shape
     
-    def get_data_columns(self) -> list:
-        return self.data.columns.tolist()
-    
-    def get_data_info(self) -> None:
-        print(f'Data shape: {self.get_data_shape()}')
-        print(f'Data columns: {self.get_data_columns()}')
-    
-    def _split_train_test(self)  -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        self.data = self._preprocess_data()
-        X = self.data.drop('class', axis=1)
+    def _split_data(self) -> None: 
+        x = self.data.drop('class', axis=1)
         y = self.data['class']
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, 
-                                                 y, 
-                                                 test_size=0.2, 
-                                                 random_state=42, 
-                                                 shuffle=True)
-        self._regularize_tensors()
-        return self.X_train, self.X_test, self.y_train, self.y_test
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.2, random_state=42, shuffle=True)
 
-    def get_data_tensors(self) -> Tuple[torch.Tensor, torch.Tensor]:
-        self._split_train_test()
-        self.X_train = torch.tensor(self.X_train.values, dtype=torch.float32)
-        self.X_test = torch.tensor(self.X_test.values, dtype=torch.float32)
+    def _scale_data(self) -> None:
+        scaler = StandardScaler()
+        self.x_train = scaler.fit_transform(self.x_train)
+        self.x_test = scaler.transform(self.x_test)
+    
+    def _get_data_tensors(self): 
+        self.x_train = torch.tensor(self.x_train, dtype=torch.float32)
+        self.x_test = torch.tensor(self.x_test, dtype=torch.float32)
         self.y_train = torch.tensor(self.y_train.values, dtype=torch.float32)
         self.y_test = torch.tensor(self.y_test.values, dtype=torch.float32)
-        return self.X_train, self.X_test, self.y_train, self.y_test
+        return self.x_train, self.x_test, self.y_train, self.y_test
+    
+    def get_dataloaders(self): 
+        self._split_data()
+        self._scale_data()
+        self._get_data_tensors()
+        train_dataset = torch.utils.data.TensorDataset(self.x_train, self.y_train)
+        test_dataset = torch.utils.data.TensorDataset(self.x_test, self.y_test)
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=self.BATCH_SIZE, shuffle=True)
+        test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=self.BATCH_SIZE, shuffle=True)
+        return train_dataloader, test_dataloader
 
-    def _regularize_tensors(self) -> None: 
-        self.X_train = (self.X_train - self.X_train.mean()) / self.X_train.std()
-        self.X_test = (self.X_test - self.X_test.mean()) / self.X_test.std()
+    # def _preprocess_data(self) -> pd.DataFrame:
+    #     self.data = self.data.dropna()
+    #     self.data = self.data.drop_duplicates()
+    #     self.data = self.data.reset_index(drop=True)
+    #     return self.data
+
+    # def get_data_shape(self) -> Tuple[int, int]:
+    #     return self.data.shape
+    
+    # def get_data_columns(self) -> list:
+    #     return self.data.columns.tolist()
+    
+    # def get_data_info(self) -> None:
+    #     print(f'Data shape: {self.get_data_shape()}')
+    #     print(f'Data columns: {self.get_data_columns()}')
+    
+    # def _split_train_test(self)  -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    #     self.data = self._preprocess_data()
+    #     X = self.data.drop('class', axis=1)
+    #     y = self.data['class']
+    #     self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, 
+    #                                                                             y, 
+    #                                                                             test_size=0.2, 
+    #                                                                             random_state=42, 
+    #                                                                             shuffle=True)
+    #     self._regularize_tensors()
+    #     return self.X_train, self.X_test, self.y_train, self.y_test
+
+    # def get_data_tensors(self) -> Tuple[torch.Tensor, torch.Tensor]:
+    #     self._split_train_test()
+    #     self.X_train = torch.tensor(self.X_train.values, dtype=torch.float32)
+    #     self.X_test = torch.tensor(self.X_test.values, dtype=torch.float32)
+    #     self.y_train = torch.tensor(self.y_train.values, dtype=torch.float32)
+    #     self.y_test = torch.tensor(self.y_test.values, dtype=torch.float32)
+    #     return self.X_train, self.X_test, self.y_train, self.y_test
+
+    # def _regularize_tensors(self) -> None: 
+    #     self.X_train = (self.X_train - self.X_train.mean()) / self.X_train.std()
+    #     self.X_test = (self.X_test - self.X_test.mean()) / self.X_test.std()
+    #     scaler = StandardScaler()
+    #     self.X_train = scaler.fit_transform(self.X_train)
+    #     self.X_test = scaler.transform(self.X_test)
